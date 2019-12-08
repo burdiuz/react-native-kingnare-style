ce9a796e8f092399a233280fa9373152a9876f37
Oleg Galaburda <burdiuz@gmail.com>
2019-12-08 19:49:40 +0200

    add value prop to FormText which will be passed as text to Text component

--- source/Form/FormText.js
+++ source/Form/FormText.js
@@ -1,6 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Text from '../Text/Text';
 import { withInputPlaceholder } from './InputPlaceholder';
 
-const FormText = withInputPlaceholder(Text, 'FormText');
+const TextValue = ({ value }) => <Text>{value}</Text>;
+
+const FormText = withInputPlaceholder(TextValue, 'FormText');
+
+FormText.propTypes = {
+  value: PropTypes.node,
+};
+
+FormText.defaultProps = {
+  value: '',
+};
 
 export default FormText;
